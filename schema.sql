09f4290ead23ea959cdcb1d22a1069b5d56b12d3
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-05-12 10:43:29 +0530

    ADD:created multi tables

--- schema.sql
+++ schema.sql
@@ -2,11 +2,28 @@
 
 CREATE TABLE animals (
     id INTEGER PRIMARY KEY NOT NULL,
-    name VARCHAR(100),
-    date_of_birth DATE,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
     escape_attempts INTEGER,
     neutered BOOLEAN,
-    weight_kg DECIMAL
+    weight_kg DECIMAL NOT NULL
 );
 
 ALTER TABLE animals ADD COLUMN species varchar(200);
+
+CREATE TABLE species (
+   id SERIAL PRIMARY KEY,
+   name varchar(100) NOT NULL
+);
+
+CREATE TABLE owners (
+   id SERIAL PRIMARY KEY,
+   full_name varchar(100) NOT NULL,
+   age int NOT NULL
+);
+
+ALTER TABLE animals
+ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY,
+DROP COLUMN species,
+ADD COLUMN species_id INTEGER REFERENCES species(id),
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
