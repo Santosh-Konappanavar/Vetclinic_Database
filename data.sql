417c3f53606f8488837709faefbde506546dcbe2
Santosh Konappanavar <siddukundaragi20@gmail.com>
2023-05-16 13:46:10 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -140,3 +140,9 @@ VALUES
     ((SELECT id FROM vets WHERE name = 'William Tatcher'),
     (SELECT id FROM animals WHERE name = 'Blossom'),
     DATE '2021-01-11');
+    
+    
+ M
+ INSERT INTO visits (animal_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+ insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
